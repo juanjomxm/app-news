55b45d60cb3db6c184c6ce82b287336068983cfc
Juan Jose A. Zuluaga <juaninfante29@gmail.com>
2024-02-01 15:49:26 -0500

    again change async/await to axios

--- src/NewsLocal.js
+++ src/NewsLocal.js
@@ -7,28 +7,23 @@ function CategoryNews(){
     const [inputSearchNewsLocal, setInputSearchNewsLocal] = React.useState('')
 
     // Axios para noticias locales
-    // const categoryAxios = axios.create({
-    //     baseURL: `https://newsapi.org/v2/top-headlines/`,
-        // params:{
-        //     'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
-        //     'q': inputSearchNewsLocal,
-        //     'category': filterCategory,
-        //     'country': 'co',
-        //     'pageSize': 10
-        // }
-    // })
+    const categoryAxios = axios.create({
+        baseURL: `https://newsapi.org/v2/top-headlines/`,
+        params:{
+            'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
+            'q': inputSearchNewsLocal,
+            'category': filterCategory,
+            'country': 'co',
+            'pageSize': 10
+        }
+    })
 
     React.useEffect(()=>{
         const viewCategory = async()=>{
-            const URL = `https://newsapi.org/v2/top-headlines/?apiKey=5a03cee5e2594c8ea66e80860c45fbba&q=${inputSearchNewsLocal}&category=${filterCategory}&country=co&pageSize=10`
-            const res = await fetch(URL, {
-                method: 'GET'
-            })
-
-            const data = await res.json()
+            const {data,status} = await categoryAxios.get()
 
             try{
-                if(res.status === 200, 201){
+                if(status === 200, 201){
                     setDataCategory(data.articles)
                 }
             }catch(error){
--- src/NewsWorld.js
+++ src/NewsWorld.js
@@ -6,41 +6,37 @@ function SearchNewsDev(){
     const [searchNews, setSearchNews] = React.useState('')
 
     // Axios para buscar las noticias que deseo
-    // const newsDev = axios.create({
-    //     baseURL: 'https://newsapi.org/v2/everything/',
-    //     params:{
-    //         'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
-    //         'q': searchNews,
-    //         'language': 'es',
-    //         'sortBy': 'publishedAt',
-    //         'pageSize': 10
-    //     }
-    // })
+    const newsDev = axios.create({
+        baseURL: 'https://newsapi.org/v2/everything/',
+        params:{
+            'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
+            'q': searchNews,
+            'language': 'es',
+            'sortBy': 'publishedAt',
+            'pageSize': 10
+        }
+    })
 
 
 
     // Axios para renderizar noticias desde el inicio
-    // const newsDevStart = axios.create({
-    //     baseURL: 'https://newsapi.org/v2/everything/',
-        // params:{
-        //     'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
-        //     'q': 'guerra',
-        //     'language': 'es',
-        //     'sortBy': 'publishedAt',
-        //     'pageSize': 10   
-        // }
-    // })
+    const newsDevStart = axios.create({
+        baseURL: 'https://newsapi.org/v2/everything/',
+        params:{
+            'apiKey': '5a03cee5e2594c8ea66e80860c45fbba',
+            'q': 'guerra',
+            'language': 'es',
+            'sortBy': 'publishedAt',
+            'pageSize': 10   
+        }
+    })
 
     // Funcion para que se renderice solo cuando busco una noticia
     const viweNewsDev = async()=>{
-        const URL = (`https://newsapi.org/v2/everything/?apiKey=5a03cee5e2594c8ea66e80860c45fbba&q=${searchNews}&language=es&sortBy=publishedAt&pageSize=10`)
-        const res = await fetch(URL, {
-            method: 'GET'
-        })
-        const data = await res.json()
+        const {data, status} = await newsDev.get()
 
         try{
-            if(res.status === 200, 201){
+            if(status === 200, 201){
                 setDataNews(data.articles)
             }
         }catch(error){
@@ -51,15 +47,10 @@ function SearchNewsDev(){
     // De esta manera puedo renderizar noticias de inmediato para que la pagina no este en lanco y despues que el usuario busque lo que desea
     React.useEffect(()=>{
         const viweNewsDevStart = async()=>{
-            const URL2 = `https://newsapi.org/v2/everything/?apiKey=5a03cee5e2594c8ea66e80860c45fbba&q=guerra&language=es&sortBy=publishedAt&pageSize=10`
-            const res = await fetch(URL2, {
-                method: 'GET'         
-            })
-
-            const data = await res.json()
+            const {data,status} = await newsDevStart.get()
 
             try{
-                if(res.status === 200, 201){
+                if(status === 200, 201){
                     setDataNews(data.articles)
                 }
             }catch(error){
